bdd06dde5e50b4be638c42858e454e832f63cb11
dmorocho <55670426+dmorocho@users.noreply.github.com>
2021-05-27 18:05:54 -0400

    nyc list added

--- src/app/screens/SearchScreen/index.js
+++ src/app/screens/SearchScreen/index.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import {Button} from '@material-ui/core'
+import './searchScreen.css'
 
 const SearchPage = () => {
 
 
     return (
-        <div className='SearchPage'>
+        <div className='searchPage'>
+            <div className='searchPage_info'>
 
-            <p>This is the search page</p>
+                <p>This is your results for burgers</p>
+            
+                <Button
+                varient='outlined'
+                >
+                price
+                </Button>
+                <Button
+                varient='outlined'
+                >
+                distance
+                </Button>
+                <Button
+                varient='outlined'
+                >
+                reviews
+                </Button>
+            </div>
+            
         </div>
 
     )
--- src/features/header/index.js
+++ src/features/header/index.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './header.css'
 import Logo from '../../img/logo.png'
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { NavLink, useHistory } from "react-router-dom";
 import LocationSearchInput from './autoplaces.js'
+import nycLocations from '../../data/index'
 
 const Header = () =>{
     const history = useHistory();
+    const [location,setLocation] = useState('')
+    const [term,setTerm] = useState('')
+    
+
+    //looks up location on change
+    const handleLocChange =(e) => {
+        e.preventDefault()
+        setLocation(e.target.value)
+        console.log({location})
+        const result = nycLocations.filter(word => word.toLowerCase().includes(location))
+         console.log(result)
+    }
+
+    const handleTermChange =(e) => {
+        e.preventDefault()
+        setTerm(e.target.value)
+
+    }
+
+    // const result = words.filter(word => word.includes('sp'));
     const handClick = () => {
 
         history.push("/search")
@@ -21,11 +42,11 @@ const Header = () =>{
                 <img className="header_icon" alt='banner_logo' src={Logo}/>   
             </NavLink>
             <div className = 'header_center'>
-                <input type='text' placeholder="where to go?"/>
+                <input type='text' placeholder="where to go?" onChange={handleLocChange}/>
                 {/* <LocationSearchInput/>  */}
                 <span>|</span>
                 <input type='text' placeholder=" what to do?"/>
-                <SearchIcon onClick={handClick}/>
+                <SearchIcon onClick={handClick} onChange={handleTermChange}/>
 
             </div>
             <div className="header_right">
